61d1077f8a3db76ae707f73d6ed132f7d7f52309
LazarUgren <laza-lazica.94@hotmail.com>
2023-05-19 16:35:54 +0200

    Unosenje prve izmene u klasu HelloWorld1

--- src/test/java/ProgrammingKnowledge/HelloWorld1.java
+++ src/test/java/ProgrammingKnowledge/HelloWorld1.java
@@ -3,5 +3,6 @@ package ProgrammingKnowledge;
 public class HelloWorld1 {
     public static void main(String[] args) {
         System.out.println("Moj prvi pokusaj");
+        System.out.println("Ovo je prva izmena");
     }
 }
